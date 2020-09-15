3de156013c27976da53c0c36bf495137fafcff40
Keenal <keenal.shah@ufl.edu>
2020-09-14 23:35:56 -0400

    add initial files

---
+++ test.fs
@@ -0,0 +1,17 @@
+let prefix string1 string2 = 
+    string1 + ", " + string2
+
+let names = ["dfafd"; "dfsaf"; "dsf"]
+
+let prefixWithHello = prefix "Hello"
+
+let exclaim s = s + "!"
+
+let bigHello = prefixWithHello >> exclaim
+
+let hellos = 
+    names
+    |> Seq.map (fun x -> printfn "Mapped with %s" x; bigHello x)
+    |> Seq.sort
+
+hellos
